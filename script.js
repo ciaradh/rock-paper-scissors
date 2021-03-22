5a412d43f3614c63c5bcad2e1ebcb8dc760a0a77
ciaradh <monicapedron23@gmail.com>
2021-03-22 22:28:51 +0100

    Initial styling

--- script.js
+++ script.js
@@ -27,8 +27,9 @@ function gameRound(playerSelection, computerSelection) {
     let playerSel = playerSelection.toLowerCase();
     document.getElementById("computerChoice").innerText = computerSelection;
     document.getElementById("playerChoice").innerText = playerSelection;
+    // document.getElementById(`p-${playerSelection}`).classList.remove("is-hidden");
     if(playerSel === computerSelection) {
-        return "It's a tie!"
+        return "It's a tie"
     }
     else if (
         (playerSel == "rock" && computerSelection == "scissors")||
@@ -37,7 +38,7 @@ function gameRound(playerSelection, computerSelection) {
         playerScore++;
         document.getElementById("playerScore").innerText = playerScore;
         displayResults();
-        return "You win!";
+        return "You win";
     } else {
         computerScore++;
         document.getElementById("computerScore").innerText = computerScore;
@@ -48,27 +49,44 @@ function gameRound(playerSelection, computerSelection) {
 
 function displayResults() {
     if(playerScore === 5) {
-        document.getElementById("winner").innerText = "Player";
-        disablePlayerChoices();
+        document.getElementById("winner").innerText = "You win!";
+        document.getElementById("playersChoices").classList.add("is-hidden");
+        document.getElementById("roundWrapper").classList.add("is-hidden");
+        document.getElementById("winnerWrapper").classList.remove("is-hidden");
+        // disablePlayerChoices();
     } else if (computerScore === 5) {
-        document.getElementById("winner").innerText = "Computer";
-        disablePlayerChoices();
+        document.getElementById("winner").innerText = "Computer wins!";
+        document.getElementById("playersChoices").classList.add("is-hidden");
+        document.getElementById("roundWrapper").classList.add("is-hidden");
+        document.getElementById("winnerWrapper").classList.remove("is-hidden");
+        // disablePlayerChoices();
     }
 }
 
-function disablePlayerChoices() {
-    let buttons = document.querySelectorAll("#playersChoices button");
-    for(let i =0; i<buttons.length; i++) {
-        buttons[i].disabled = true;
-    }
-}
+// function disablePlayerChoices() {
+//     let buttons = document.querySelectorAll("#playersChoices button");
+//     for(let i =0; i<buttons.length; i++) {
+//         buttons[i].disabled = true;
+//     }
+// }
 
 function getPlayerSelection(button) {
+    //hide players selection wrapper and show round wrapper
+    document.getElementById("playersChoices").classList.add("is-hidden");
+    document.getElementById("roundWrapper").classList.remove("is-hidden");
+    
     playerSelection = button.value;
     let round = gameRound(playerSelection, computerPlay());
-    document.getElementById("roundWinner").innerText = round;
+    roundWinner.innerText = round;
+    
 }
 
 function resetGame() {
     window.location.reload();
+}
+
+function newRound() {
+    //TODO show choice wrapper, hide result wrapper
+    document.getElementById("playersChoices").classList.remove("is-hidden");
+    document.getElementById("roundWrapper").classList.add("is-hidden");
 }
